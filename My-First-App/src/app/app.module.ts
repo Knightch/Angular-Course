31d50c9bc4a69a941c9ad363ae96374296c765c2
knightCh <amankumarpandey2397@gmail.com>
2021-05-08 20:54:10 +0530

    Creating Component for Real project

--- My-First-App/src/app/app.module.ts
+++ My-First-App/src/app/app.module.ts
@@ -4,10 +4,25 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { RecipesComponent } from './recipes/recipes.component';
+import { HeaderComponent } from './header/header.component';
+import { RecipesDetailsComponent } from './recipes/recipes-details/recipes-details.component';
+import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
+import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipes-item.component';
+import { ShoppingComponent } from './shopping/shopping.component';
+import { ShoppingEditComponent } from './shopping/shopping-edit/shopping-edit.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    RecipesComponent,
+    HeaderComponent,
+    RecipesDetailsComponent,
+    RecipesListComponent,
+    RecipesItemComponent,
+    ShoppingComponent,
+    ShoppingEditComponent
+
   ],
   imports: [
     BrowserModule,
