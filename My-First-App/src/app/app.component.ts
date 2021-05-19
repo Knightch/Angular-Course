35a69176b078995a654f597feb9193f16b58ef7a
knightCh <amankumarpandey2397@gmail.com>
2021-05-19 20:17:11 +0530

    using ngContent

--- My-First-App/src/app/app.component.ts
+++ My-First-App/src/app/app.component.ts
@@ -6,25 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  serverElements = [{name:'NewserverAman', type:'server',content:'it is a testing server' }]
+  serverElements = [{ name: 'NewserverAman', type: 'server', content: 'it is a testing server' }]
 
 
-   onServerAdded(serverData:{serverName: string, serverContent:string}) {
-       this.serverElements.push({
-           type:'server',
-           name:serverData.serverName,
-           content: serverData.serverContent
-         });
-    }
+  onServerAdded(serverData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
 
-    onBlueprintAdded(blueprintData:{serverName: string, serverContent:string}) {
-      this.serverElements.push({
-          type:'blueprint',
-          name:blueprintData.serverName,
-          content:blueprintData.serverContent
-        });
-   }
+  onBlueprintAdded(blueprintData: { serverName: string, serverContent: string }) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
 }
-   
+
 
 
