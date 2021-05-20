d68e6a5b9159769b9b3bb4717b474ee5475797bb
knightCh <amankumarpandey2397@gmail.com>
2021-05-20 20:18:56 +0530

    using hooks  1. DoCheck 2.AfterContentInit 3.AfterContentChecked 4.AfterViewInit 5.AfterViewChecked

--- My-First-App/src/app/server-element/server-element.component.ts
+++ My-First-App/src/app/server-element/server-element.component.ts
@@ -1,11 +1,29 @@
-import { Component, Input, OnInit, OnChanges, SimpleChanges, Output } from '@angular/core';
+import {
+  Component,
+  Input,
+  OnInit,
+  OnChanges,
+  SimpleChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked
+} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit, OnChanges {
+export class ServerElementComponent implements OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewInit,
+  AfterViewChecked {
   @Input('srvrElement') element: { name: string, type: string, content: string };
   @Input() name: string;
   constructor() {
@@ -20,5 +38,26 @@ export class ServerElementComponent implements OnInit, OnChanges {
   ngOnInit() {
     console.log("ngOnInit called!");
   }
+
+  ngDoCheck() {
+    console.log("ngDoCheck called!");
+  }
+
+  ngAfterContentInit() {
+    console.log("AfterContentInit called!");
+  }
+
+  ngAfterContentChecked() {
+    console.log("AfterContentChecked called!");
+  }
+
+  ngAfterViewInit() {
+    console.log("AfterViewInit called!");
+  }
+
+  ngAfterViewChecked() {
+    console.log("AfterViewChecked called!");
+  }
+
 
 }
