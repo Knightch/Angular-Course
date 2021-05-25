28bd335158333ff26905dcdcb1882c546c4c2f67
knightCh <amankumarpandey2397@gmail.com>
2021-05-25 15:00:47 +0530

    binding to directive properties

--- My-First-App/src/app/BetterHighlight/better-high-light.directive.ts
+++ My-First-App/src/app/BetterHighlight/better-high-light.directive.ts
@@ -1,9 +1,22 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  HostBinding,
+  HostListener,
+  Input,
+  OnInit,
+  Renderer2
+} from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighLight]'
 })
 export class BetterHighLightDirective implements OnInit {
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightColor: string = 'blue';
+
+  @HostBinding('style.backgroundColor') backgroundColor: string = this.highlightColor;
+  @HostBinding('style.color') textColor: string = this.highlightColor;
 
   constructor(private elRef: ElementRef, private renderer: Renderer2) { }
   ngOnInit() {
@@ -11,11 +24,15 @@ export class BetterHighLightDirective implements OnInit {
     //this.renderer.setStyle(this.elRef.nativeElement, 'color', '#FFFFFF');
   }
   @HostListener('mouseenter') mouseOver(EventData:Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue');
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', '#FFFFFF');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', '#FFFFFF');
+    this.backgroundColor = this.highlightColor;
+    this.textColor = this.defaultColor;
   }
   @HostListener('mouseleave') mouseLeave(EventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'yellow');
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', '#FFFFFF');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'yellow');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', '#FFFFFF');
+    this.backgroundColor = this.highlightColor;
+    this.textColor = this.defaultColor;
   }
 }
