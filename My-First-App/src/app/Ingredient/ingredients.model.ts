b722f40123ee95edb38737f25ad1f298877f8636
knightCh <amankumarpandey2397@gmail.com>
2021-05-27 21:47:50 +0530

    A Example Project

--- My-First-App/src/app/Ingredient/ingredients.model.ts
+++
@@ -1,5 +0,0 @@
-export class Ingrediant{
-    constructor(public name: string, public amount: number){
-        
-  }
-}
--- My-First-App/src/app/app.component.ts
+++ My-First-App/src/app/app.component.ts
@@ -6,9 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  loadedFeature = 'recipe';
-  
-  onNavigate(feature:string) {
-    this.loadedFeature = feature;
+  accounts = [
+    {
+      name: 'Master Account',
+      status: 'acive'
+    },
+    {
+      name: 'Testaccount',
+      status: 'inactive'
+    },
+    {
+      name: 'Hidden Account',
+      status: 'unknown'
+    }
+  ];
+
+  onAccountAdded(newAccount: { name: string, status: string }) {
+    this.accounts.push(newAccount);
+  }
+  onStatusChanged(updateInfo: { id: number, newStatus: string }) {
+    this.accounts[updateInfo.id];
+    status = updateInfo.newStatus;
   }
 }
