60ecd33b5f9c3b6c2a0dbb511e870c921318fcf8
knightCh <amankumarpandey2397@gmail.com>
2021-05-28 12:54:17 +0530

    Injecting the logging Service into Component

--- My-First-App/src/app/new-account/new-account.component.ts
+++ My-First-App/src/app/new-account/new-account.component.ts
@@ -4,21 +4,21 @@ import { LoggingService } from '../logging.service';
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
 })
 export class NewAccountComponent implements OnInit {
-   @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
+  @Output() accountAdded = new EventEmitter<{ name: string, status: string }>();
 
-  constructor() { }
+  constructor(private loggingService: LoggingService) { }
 
   ngOnInit(): void {
   }
-  onCreatAccount(accountName:string,accountStatus:string) {
+  onCreatAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
-      status:accountStatus
+      status: accountStatus
     });
-    const service = new LoggingService();
-    service.logStatusCHanges(accountStatus);
+    this.loggingService.logStatusCHanges(accountStatus);
   }
 }
