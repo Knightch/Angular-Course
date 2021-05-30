dab096261b915123dbb817ec7a27d9420c03485b
knightCh <amankumarpandey2397@gmail.com>
2021-05-30 16:38:49 +0530

    Adding Ingredient to recipes

--- My-First-App/src/app/recipes/receipes.model.ts
+++ My-First-App/src/app/recipes/receipes.model.ts
@@ -1,11 +1,15 @@
+import { Ingrediant } from "../Ingredient/ingredients.model";
+
 export class Recipe{
     public name: string;
     public description: string;
-    public imagePath: string;
+  public imagePath: string;
+  public ingrediants: Ingrediant [];
 
-    constructor(name: string, desc: string, imagePath: string) {
+    constructor(name: string, desc: string, imagePath: string, ingrediants: Ingrediant[]) {
       this.name = name;
       this.description = desc;
       this.imagePath = imagePath;
+      this.ingrediants = ingrediants;
     }
 }
--- My-First-App/src/app/recipes/recipe.service.ts
+++ My-First-App/src/app/recipes/recipe.service.ts
@@ -1,12 +1,34 @@
 import { EventEmitter } from "@angular/core";
+import { Ingrediant } from "../Ingredient/ingredients.model";
 import { Recipe } from "./receipes.model";
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     private recipes: Recipe[] = [
-        new Recipe('Biryani', 'A nice biryani that i love most', 'https://embed.widencdn.net/img/mccormick/mjdxosbfzz/1125x1125px/HYDERABADI-BIRYANI.jpg?crop=true&anchor=749,0&q=80&color=ffffffff&u=qwwekl'),
-        new Recipe('Biryani', 'A nice biryani that i love most', 'https://img-global.cpcdn.com/recipes/991bc444a26af47e/751x532cq70/tandoori-chicken-biryani-recipe-main-photo.jpg '),
-        new Recipe('Ice-Cream', ' i love ice-cream', 'https://images.herzindagi.info/image/2020/Jun/chocolate-parle-g-ice-cream.jpg ')
+        new Recipe('Biryani',
+            'Hydrabadi Biryani',
+            'https://embed.widencdn.net/img/mccormick/mjdxosbfzz/1125x1125px/HYDERABADI-BIRYANI.jpg?crop=true&anchor=749,0&q=80&color=ffffffff&u=qwwekl',
+            [
+                new Ingrediant('Rice', 2),
+                new Ingrediant('Meat', 1),
+                new Ingrediant('Spices', 2)
+            ]),
+        new Recipe('Biryani',
+            'A nice biryani that i love most',
+            'https://img-global.cpcdn.com/recipes/991bc444a26af47e/751x532cq70/tandoori-chicken-biryani-recipe-main-photo.jpg ',
+            [
+                new Ingrediant('Rice', 1),
+                new Ingrediant('Meat', 1),
+                new Ingrediant('Spices', 1)
+            ]),
+        new Recipe('Ice-Cream',
+            ' i love ice-cream',
+            'https://images.herzindagi.info/image/2020/Jun/chocolate-parle-g-ice-cream.jpg ',
+            [
+                new Ingrediant('Cream', 1),
+                new Ingrediant('Milk', 1),
+                new Ingrediant('Ice', 1)
+            ])
     ];
 
     getRecipe() {
