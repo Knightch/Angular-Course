36dd681f020550a3da37e8c1a8996ff8bb16f3be
knightCh <amankumarpandey2397@gmail.com>
2021-06-05 13:44:11 +0530

    passing parameter and fatching route parameters

--- My-First-App/src/app/users/users.component.ts
+++ My-First-App/src/app/users/users.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../user.model';
 
 @Component({
   selector: 'app-users',
@@ -7,6 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
+  users: User[] = [
+    { id: 1, name: 'Abhinash' },
+    { id: 2, name: 'Adil' },
+    { id: 3, name: 'Anupriya' },
+    { id: 4, name: 'Srija' },
+    { id: 5, name: 'Gaurav' },
+    { id: 6, name: 'Deepak' },
+    { id: 7, name: 'Manohar' },
+    { id: 8, name: 'Sonali' }
+
+  ]
   constructor() { }
 
   ngOnInit(): void {
