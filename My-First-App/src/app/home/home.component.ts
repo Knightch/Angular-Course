e454a409d1d503081fe1206f57903d4a9db7cd47
knightCh <amankumarpandey2397@gmail.com>
2021-06-16 14:30:23 +0530

    error and completion

--- My-First-App/src/app/home/home.component.ts
+++ My-First-App/src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { interval, Subscription } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -11,13 +11,40 @@ export class HomeComponent implements OnInit, OnDestroy {
   constructor() { }
 
   ngOnInit() {
-    this.firstObsSubscription = interval(1000).subscribe(count => {
-      console.log(count)
-    });
+    // this.firstObsSubscription = interval(1000).subscribe(count => {
+    //   console.log(count)
+    // });
+    const customIntervalObserver = Observable.create(
+      observer => {
+        let count = 0;
+        setInterval(() => {
+          observer.next(count);
+          if (count === 2) {
+            observer.complete();
+          }
+          if (count > 3) {
+            observer.error(new Error('Count is greater 3!'));
+          }
+          count++;
+        }, 1000);
+      }
+    );
+
+    this.firstObsSubscription = customIntervalObserver.subscribe(
+      data => {
+        console.log(data);
+      }, error => {
+        console.log(error);
+        alert(error.message)
+    }, () => {
+        console.log('completed');
+      }
+    )
   }
   ngOnDestroy() {
     this.firstObsSubscription.unsubscribe();
   }
 
 }
+
 
