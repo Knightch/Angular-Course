cde4a4505bdd4275cad8a098c808585f49f96a0b
knightCh <amankumarpandey2397@gmail.com>
2021-06-18 13:49:46 +0530

    creating form

--- My-First-App/src/app/app.module.ts
+++ My-First-App/src/app/app.module.ts
@@ -4,40 +4,20 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { RecipesComponent } from './recipes/recipes.component';
-import { HeaderComponent } from './header/header.component';
-import { RecipesDetailsComponent } from './recipes/recipes-details/recipes-details.component';
-import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
-import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipes-item.component';
-import { ShoppingComponent } from './shopping/shopping.component';
-import { ShoppingEditComponent } from './shopping/shopping-edit/shopping-edit.component';
-import { ShoppingService } from './shopping/shopping.service';
-import { DropDownDirective } from './Ingredient/dropdown.directive';
-import { AppRoutingModule } from './app-routing.module';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    RecipesComponent,
-    HeaderComponent,
-    RecipesDetailsComponent,
-    RecipesListComponent,
-    RecipesItemComponent,
-    ShoppingComponent,
-    ShoppingEditComponent,
-    DropDownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent
+   
 
   ],
   imports: [
     BrowserModule,
-    FormsModule,
-    AppRoutingModule
+    FormsModule
+  
   ],
-  providers: [ShoppingService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
