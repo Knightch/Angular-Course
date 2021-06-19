9fd990e31343e69b101d4a400aeb9e2461f92453
knightCh <amankumarpandey2397@gmail.com>
2021-06-19 23:51:48 +0530

    Creating a form in code &  syncing HTML and form

--- My-First-App/src/app/app.component.ts
+++ My-First-App/src/app/app.component.ts
@@ -1,12 +1,21 @@
+import { OnInit } from '@angular/core';
 import { Component, ViewChild } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   genders = ['Male', 'Female'];
   signUpForm: FormGroup;
+
+  ngOnInit() {
+    this.signUpForm = new FormGroup({
+      'username': new FormControl(null),
+      'mail': new FormControl(null),
+      'gender': new FormControl('Male')
+    });
+  }
 }
