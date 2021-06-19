1b678476d3e3460e0a318258ea5e1df2a16a380c
knightCh <amankumarpandey2397@gmail.com>
2021-06-19 23:28:14 +0530

    Reacting form setup & using resote in Template Form

--- My-First-App/src/app/app.component.ts
+++ My-First-App/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,53 +7,6 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  @ViewChild('f') formSubmit: NgForm;
-
-  defaultValue = "pet";
-  defaultName = "Enter your name";
-  defaultEmail = "Enter your mail";
-  answer = "";
   genders = ['Male', 'Female'];
-  user = {
-    name: '',
-    mail: '',
-    secret: '',
-    answer: '',
-    gender: ''
-  }
-  submitDetails = false;
-
-  //   onSubmit(form:NgForm) {
-  //     console.log(form);
-  //  }
-
-  suggestUserName() {
-    const suggestUserName = 'Knight';
-    // this.formSubmit.setValue(
-    //   {
-    //     userData: {
-    //       userName: suggestUserName,
-    //       email:'amankumarpander2397@gmail.com'
-    //     },
-    //     secret: 'teacher',
-    //     answer: 'Ambuj Sir',
-    //     gender: 'Male'
-
-    //   }
-    // );
-    this.formSubmit.form.patchValue({
-      userData: {
-        userName: suggestUserName
-      }
-    })
-  }
-
-  onSubmit() {
-    this.submitDetails = true;
-    this.user.name = this.formSubmit.value.userData.userName;
-    this.user.mail = this.formSubmit.value.userData.email;
-    this.user.secret = this.formSubmit.value.secret;
-    this.user.answer = this.formSubmit.value.answer;
-    this.user.gender = this.formSubmit.value.gender;
-  }
+  signUpForm: FormGroup;
 }
--- My-First-App/src/app/app.module.ts
+++ My-First-App/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 
 import { AppComponent } from './app.component';
@@ -8,14 +9,11 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-   
-
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
-  
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
