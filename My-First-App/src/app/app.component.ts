aec5d886ebe897ceccda1fcc6066a06a3f3b71c8
knightCh <amankumarpandey2397@gmail.com>
2021-06-19 14:41:43 +0530

    setValue vs PatchValue

--- My-First-App/src/app/app.component.ts
+++ My-First-App/src/app/app.component.ts
@@ -8,14 +8,37 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') formSubmit: NgForm;
+
   defaultValue = "pet";
   defaultName = "Enter your name";
   defaultEmail = "Enter your mail";
   answer = "";
   genders = ['Male', 'Female'];
+
 //   onSubmit(form:NgForm) {
 //     console.log(form);
 //  }
+  
+  suggestUserName() {
+    const suggestUserName = 'Knight';
+    // this.formSubmit.setValue(
+    //   {
+    //     userData: {
+    //       userName: suggestUserName,
+    //       email:'amankumarpander2397@gmail.com'
+    //     },
+    //     secret: 'teacher',
+    //     answer: 'Ambuj Sir',
+    //     gender: 'Male'
+        
+    //   }
+    // );
+    this.formSubmit.form.patchValue({
+      userData: {
+        userName: suggestUserName
+      }
+    })
+  }
   
   onSubmit() {
     console.log(this.formSubmit);
