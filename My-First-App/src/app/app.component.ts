cf10fd0c06d49ab4f99904ecf5951b426ed8173a
knightCh <amankumarpandey2397@gmail.com>
2021-06-20 12:34:13 +0530

    FormArrays and Creating custom Validators

--- My-First-App/src/app/app.component.ts
+++ My-First-App/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { OnInit } from '@angular/core';
-import { Component, ViewChild } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component } from '@angular/core';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -10,17 +10,31 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 export class AppComponent implements OnInit {
   genders = ['Male', 'Female'];
   signUpForm: FormGroup;
+  forbiddenUsername = ['Sujit', 'Jitu', 'Sourav', 'Raj', 'Amarjit', 'Raja'];
 
   ngOnInit() {
     this.signUpForm = new FormGroup({
       'userData': new FormGroup({
-        'username': new FormControl(null, Validators.required),
+        'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
         'mail': new FormControl(null, [Validators.required, Validators.email])
       }),
-      'gender': new FormControl('Male')
+      'gender': new FormControl('Male'),
+      'hobbies': new FormArray([])
     });
   }
   onSubmit() {
     console.log(this.signUpForm);
+  }
+
+  addHobbies() {
+    const control = new FormControl(null, Validators.required);
+    (<FormArray>this.signUpForm.get('hobbies')).push(control);
+  }
+
+  forbiddenNames(control: FormControl): { [s: string]: boolean } {
+    if (this.forbiddenUsername.indexOf(control.value) !== -1) {
+      return { 'nameIsForbidden': true };
+    }
+    return null;
   }
 }
