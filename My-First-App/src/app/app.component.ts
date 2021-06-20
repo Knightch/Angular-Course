0105adc5528b47f7d330f682ce91ec6db46e03be
knightCh <amankumarpandey2397@gmail.com>
2021-06-20 13:29:33 +0530

    Reactive a Custom Asynchronous Validators

--- My-First-App/src/app/app.component.ts
+++ My-First-App/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { OnInit } from '@angular/core';
 import { Component } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -16,7 +17,7 @@ export class AppComponent implements OnInit {
     this.signUpForm = new FormGroup({
       'userData': new FormGroup({
         'username': new FormControl(null, [Validators.required, this.forbiddenNames.bind(this)]),
-        'mail': new FormControl(null, [Validators.required, Validators.email])
+        'mail': new FormControl(null, [Validators.required, Validators.email], this.forbiddenEmails)
       }),
       'gender': new FormControl('Male'),
       'hobbies': new FormArray([])
@@ -36,5 +37,18 @@ export class AppComponent implements OnInit {
       return { 'nameIsForbidden': true };
     }
     return null;
+  }
+
+  forbiddenEmails(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'amankumarpandey2397@gmail.com') {
+          resolve({ 'emailIsForbidden': true });
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
   }
 }
