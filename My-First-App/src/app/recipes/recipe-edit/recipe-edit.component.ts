461f6c32ce514391aa74172590796d30ea3298b0
knightCh <amankumarpandey2397@gmail.com>
2021-06-21 00:35:47 +0530

    Adding new ingredientb controls

--- My-First-App/src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ My-First-App/src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -27,6 +27,15 @@ export class RecipeEditComponent implements OnInit {
     console.log(this.recipeForm);
   }
 
+  onAddIngredients() {
+    (<FormArray>this.recipeForm.get('ingredients')).push(
+      new FormGroup({
+        'name': new FormControl(),
+        'amount': new FormControl()
+      })
+    )
+  }
+
   private initForm() {
     let recipeName = '';
     let recipeImagePath = '';
