f0d49f247d1b1ea858528ed74088cdcc923f9ff0
knightCh <amankumarpandey2397@gmail.com>
2021-06-21 16:16:48 +0530

    Using Pipes

--- My-First-App/src/app/app.module.ts
+++ My-First-App/src/app/app.module.ts
@@ -4,42 +4,17 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { HeaderComponent } from './header/header.component';
-import { RecipesDetailsComponent } from './recipes/recipes-details/recipes-details.component';
-import { RecipesListComponent } from './recipes/recipes-list/recipes-list.component';
-import { RecipesItemComponent } from './recipes/recipes-list/recipes-item/recipes-item.component';
-import { ShoppingComponent } from './shopping/shopping.component';
-import { ShoppingEditComponent } from './shopping/shopping-edit/shopping-edit.component';
-import { ShoppingService } from './shopping/shopping.service';
-import { DropDownDirective } from './Ingredient/dropdown.directive';
-import { AppRoutingModule } from './app-routing.module';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
-import { RecipeService } from './recipes/recipe.service';
-
 @NgModule({
   declarations: [
-    AppComponent,
-    RecipesComponent,
-    HeaderComponent,
-    RecipesDetailsComponent,
-    RecipesListComponent,
-    RecipesItemComponent,
-    ShoppingComponent,
-    ShoppingEditComponent,
-    DropDownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent
+    AppComponent
 
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule,
-    AppRoutingModule
+    ReactiveFormsModule
   ],
-  providers: [ShoppingService,RecipeService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
