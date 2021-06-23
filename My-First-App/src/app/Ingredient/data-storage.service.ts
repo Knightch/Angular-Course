c76ba1d5e5fb5d7c68e80aa4d078421c365b21a4
knightCh <amankumarpandey2397@gmail.com>
2021-06-23 23:42:14 +0530

    transforming fatching data

--- My-First-App/src/app/Ingredient/data-storage.service.ts
+++ My-First-App/src/app/Ingredient/data-storage.service.ts
@@ -2,13 +2,14 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Recipe } from "../recipes/receipes.model";
 import { RecipeService } from "../recipes/recipe.service";
+import { map } from "rxjs/operators";
 
 @Injectable({
-    providedIn:'root'
+    providedIn: 'root'
 })
-export class DataStorageService{
+export class DataStorageService {
     constructor(private http: HttpClient, private recipeService: RecipeService) { }
-        
+
     storageRecipes() {
         const recipes = this.recipeService.getRecipe();
         this.http.put('https://ng-course-recipe-book-5c3ee-default-rtdb.firebaseio.com/recipes.json', recipes).subscribe(
@@ -18,11 +19,17 @@ export class DataStorageService{
         )
     }
     fetchRecipe() {
-        this.http.get<Recipe[]>('https://ng-course-recipe-book-5c3ee-default-rtdb.firebaseio.com/recipes.json').subscribe(
-            recipes => {
-                this.recipeService.setRecipes(recipes);
-            }
-        )
+        this.http.get<Recipe[]>('https://ng-course-recipe-book-5c3ee-default-rtdb.firebaseio.com/recipes.json')
+            .pipe(map(recipes => {
+                return recipes.map(recipe => {
+                    return { ...recipe, ingrediants: recipe.ingrediants ? recipe.ingrediants : [] };
+                });
+            }))
+            .subscribe(
+                recipes => {
+                    this.recipeService.setRecipes(recipes);
+                }
+            )
     }
 
 }
--- My-First-App/src/app/recipes/recipe.service.ts
+++ My-First-App/src/app/recipes/recipe.service.ts
@@ -6,32 +6,33 @@ import { Recipe } from "./receipes.model";
 @Injectable()
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
-    private recipes: Recipe[] = [
-        new Recipe('Biryani',
-            'Hydrabadi Biryani',
-            'https://embed.widencdn.net/img/mccormick/mjdxosbfzz/1125x1125px/HYDERABADI-BIRYANI.jpg?crop=true&anchor=749,0&q=80&color=ffffffff&u=qwwekl',
-            [
-                new Ingrediant('Rice', 2),
-                new Ingrediant('Meat', 1),
-                new Ingrediant('Spices', 2)
-            ]),
-        new Recipe('Biryani',
-            'A nice biryani that i love most',
-            'https://img-global.cpcdn.com/recipes/991bc444a26af47e/751x532cq70/tandoori-chicken-biryani-recipe-main-photo.jpg ',
-            [
-                new Ingrediant('Rice', 1),
-                new Ingrediant('Meat', 1),
-                new Ingrediant('Spices', 1)
-            ]),
-        new Recipe('Ice-Cream',
-            ' i love ice-cream',
-            'https://images.herzindagi.info/image/2020/Jun/chocolate-parle-g-ice-cream.jpg ',
-            [
-                new Ingrediant('Cream', 1),
-                new Ingrediant('Milk', 1),
-                new Ingrediant('Ice', 1)
-            ])
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe('Biryani',
+    //         'Hydrabadi Biryani',
+    //         'https://embed.widencdn.net/img/mccormick/mjdxosbfzz/1125x1125px/HYDERABADI-BIRYANI.jpg?crop=true&anchor=749,0&q=80&color=ffffffff&u=qwwekl',
+    //         [
+    //             new Ingrediant('Rice', 2),
+    //             new Ingrediant('Meat', 1),
+    //             new Ingrediant('Spices', 2)
+    //         ]),
+    //     new Recipe('Biryani',
+    //         'A nice biryani that i love most',
+    //         'https://img-global.cpcdn.com/recipes/991bc444a26af47e/751x532cq70/tandoori-chicken-biryani-recipe-main-photo.jpg ',
+    //         [
+    //             new Ingrediant('Rice', 1),
+    //             new Ingrediant('Meat', 1),
+    //             new Ingrediant('Spices', 1)
+    //         ]),
+    //     new Recipe('Ice-Cream',
+    //         ' i love ice-cream',
+    //         'https://images.herzindagi.info/image/2020/Jun/chocolate-parle-g-ice-cream.jpg ',
+    //         [
+    //             new Ingrediant('Cream', 1),
+    //             new Ingrediant('Milk', 1),
+    //             new Ingrediant('Ice', 1)
+    //         ])
+    // ];
+    private recipes: Recipe[]=[];
     constructor(private slService: ShoppingService) { }
 
     setRecipes(recipes: Recipe[]) {
