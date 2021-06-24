c155ba114abbd0a20b63fb24ee975cf3f5e9a384
knightCh <amankumarpandey2397@gmail.com>
2021-06-24 23:29:26 +0530

    Sending Login Request

--- My-First-App/src/app/Auth/auth.service.ts
+++ My-First-App/src/app/Auth/auth.service.ts
@@ -3,12 +3,13 @@ import { Injectable } from "@angular/core";
 import { throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
 
-interface AuthResponseData {
+export interface AuthResponseData {
     idToken: string,
     email: string,
     refreshToken: string,
     expiresin: string,
-    localled: string
+    localled: string,
+    registered?: boolean
 }
 
 @Injectable({
@@ -36,5 +37,14 @@ export class AuthService {
                 }
                 return throwError(errorMessage);
             }))
+    }
+    login(email: string, password: string) {
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCc8t04BN1B7JTIO2JrEzYgD8QWVBOAF_k',
+            {
+                email: email,
+                password: password,
+                returnSecureToken: true
+            }
+        )
     }
 }
