393a3b5223d7892bd8ab3554b99cecd96ed09c5e
knightCh <amankumarpandey2397@gmail.com>
2021-06-26 13:14:50 +0530

    Reflecting the Authentication State in the user Interface

--- My-First-App/src/app/header/header.component.ts
+++ My-First-App/src/app/header/header.component.ts
@@ -1,4 +1,7 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { subscribeOn } from 'rxjs/operators';
+import { AuthService } from '../Auth/auth.service';
 import { DataStorageService } from '../Ingredient/data-storage.service';
 
 @Component({
@@ -6,18 +9,25 @@ import { DataStorageService } from '../Ingredient/data-storage.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
+  isAuthentication = false;
+  private authSub: Subscription;
 
-  constructor(private dataStorageService: DataStorageService) { }
-  
+  constructor(private dataStorageService: DataStorageService, private authService: AuthService) { }
+
   ngOnInit() {
-    
+    this.authSub = this.authService.user.subscribe((user => {
+      this.isAuthentication = true;
+    }));
   }
   onSaveData() {
     this.dataStorageService.storageRecipes();
- }
+  }
   onFatchData() {
     this.dataStorageService.fetchRecipe().subscribe();
+  }
+  ngOnDestroy() {
+    this.authSub.unsubscribe();
   }
 
 }
