de48c8688d6edf0da2fb752e0d3c2977c99874b3
knightCh <amankumarpandey2397@gmail.com>
2021-06-29 14:15:40 +0530

    Adding an Authentication Featuren Module

--- My-First-App/src/app/app-routing.module.ts
+++ My-First-App/src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-import { AuthComponent } from "./Auth/auth.component";
+
 
 const appRoutes: Routes = [
     { path: '', redirectTo: '/recipes', pathMatch: 'full' }
